ccca1f4b04141ef615ff334c0a9c4fe15bd6b53d
m1l3m <kolfild93@gmail.com>
2023-04-27 10:14:15 +0200

    Core logic

--- index.js
+++ index.js
@@ -1,18 +1,117 @@
 const core = require('@actions/core');
-const wait = require('./wait');
+const github = require('@actions/github');
 
 
 // most @actions toolkit packages have async methods
 async function run() {
   try {
-    const ms = core.getInput('milliseconds');
-    core.info(`Waiting ${ms} milliseconds ...`);
 
-    core.debug((new Date()).toTimeString()); // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
-    await wait(parseInt(ms));
-    core.info((new Date()).toTimeString());
+    const owner=core.getInput('owner',{required:true});
+    const repo=core.getInput('repo',{required:true});
+    const token=core.getInput('token',{required:true});
+    const pr_number=core.getInput('pr_number',{required:true});
 
-    core.setOutput('time', new Date().toTimeString());
+    const octokit =new github.getOctokit(token); 
+
+    const {data:changedFiles} = await octokit.rest.pulls.listFiles({  
+      owner,      
+      repo,
+      pull_number:pr_number,      
+    });     
+
+    let diffData   = {
+      additions: 0,
+      deletions: 0,
+      changes: 0
+    };
+
+    await octokit.rest.issues.createComment({
+      owner,
+      repo,
+      issue_number:pr_number,
+      body:`
+      Pull request #${pr_number} has been updated with:\n
+      ## Changes
+      ${diffData.changes}\n
+      ## Additions
+      ${diffData.additions}\n      
+      ## Deletions
+      ${diffData.deletions}\n
+      `
+    });
+
+
+    diffData=changedFiles.reduce((acc,file)=>{
+      acc.additions=file.additions;
+      acc.deletions=file.deletions;
+      acc.changes=file.changes;
+      return acc;
+    },diffData);
+
+    for (const file of changedFiles) {
+      const fileExtension = file.filename.split('.').pop();
+      let label='';
+      switch (fileExtension) {
+        case 'js':
+          label='JavaScript';
+          break;
+        case 'css':
+          label='CSS';
+          break;
+        case 'html':
+          label='HTML';
+          break;
+        case 'json':
+          label='JSON';
+          break;
+        case 'md':
+          label='Markdown';
+          break;
+        case 'yml':
+          label='YAML';
+          break;
+        case 'xml':
+          label='XML';
+          break;
+        case 'sh':
+          label='Shell Script';
+          break;
+        case 'py':
+          label='Python';
+          break;
+        case 'java':
+          label='Java';
+          break;
+        case 'kt':
+          label='Kotlin';
+          break;
+        case 'go':
+          label='Go';
+          break;
+        case 'rb':
+          label='Ruby';
+          break;
+        case 'php':
+          label='PHP';
+          break;
+        case 'cpp':
+          label='C++';
+          break;
+        case 'cs':
+          label='C#';
+          break;
+        default:
+          label='Unknown';
+      }
+
+      await octokit.rest.issues.addLabel({
+        owner,
+        repo,        
+        issue_number:pr_number,
+        labels:[label]
+      });
+    }
+
   } catch (error) {
     core.setFailed(error.message);
   }
